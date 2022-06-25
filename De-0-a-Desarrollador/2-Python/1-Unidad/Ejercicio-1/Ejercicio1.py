9f853186efdaa93c4f4b7ed325d35ca965c52ee1
JCSIVO <josecsivo@gmail.com>
2022-06-25 11:45:26 +0200

    Ejercicio1-2
    Se suben los ejercicios 1-2 de la unidad 1 de python

---
+++ De-0-a-Desarrollador/2-Python/1-Unidad/Ejercicio-1/Ejercicio1.py
@@ -0,0 +1,24 @@
+Last login: Sat Jun 25 11:18:55 on console
+
+The default interactive shell is now zsh.
+To update your account to use zsh, please run `chsh -s /bin/zsh`.
+For more details, please visit https://support.apple.com/kb/HT208050.
+MBP-de-Jose:~ joseconejerosivo$ python3
+Python 3.8.9 (default, Apr 13 2022, 08:48:07) 
+[Clang 13.1.6 (clang-1316.0.21.2.5)] on darwin
+Type "help", "copyright", "credits" or "license" for more information.
+>>> pinnt("Hola soy Jose")
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+NameError: name 'pinnt' is not defined
+>>> print("Hola soy Jose")
+Hola soy Jose
+>>> print("Estoy empezando el curso de python 2022")
+Estoy empezando el curso de python 2022
+>>> print("Espero apredner mucho con el profesor Victor")
+Espero apredner mucho con el profesor Victor
+>>> 
+
+
+
+
---
+++ De-0-a-Desarrollador/2-Python/1-Unidad/Ejercicio-2/Ejercicio2.py
@@ -0,0 +1,24 @@
+Last login: Sat Jun 25 11:19:18 on ttys000
+
+The default interactive shell is now zsh.
+To update your account to use zsh, please run `chsh -s /bin/zsh`.
+For more details, please visit https://support.apple.com/kb/HT208050.
+MBP-de-Jose:~ joseconejerosivo$ python3
+Python 3.8.9 (default, Apr 13 2022, 08:48:07) 
+[Clang 13.1.6 (clang-1316.0.21.2.5)] on darwin
+Type "help", "copyright", "credits" or "license" for more information.
+>>> saludo = "Hola mundo!"
+>>> print("saludo")
+saludo
+>>> print(saludo)
+Hola mundo!
+>>> 
+
+
+
+
+
+
+
+
+
