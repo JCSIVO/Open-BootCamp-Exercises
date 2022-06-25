3c2fe725a2ce3bb1eaf9257da7b185f2c797d2b0
JCSIVO <josecsivo@gmail.com>
2022-06-25 13:32:55 +0200

    Ejercicio-unidad2
    Realización de los ejercicios de la unidad 2

---
+++ De-0-a-Desarrollador/2-Python/2-Unidad/Ejercicio-1/Ejercicio1.py
@@ -0,0 +1,24 @@
+Last login: Sat Jun 25 11:56:23 on ttys000
+
+The default interactive shell is now zsh.
+To update your account to use zsh, please run `chsh -s /bin/zsh`.
+For more details, please visit https://support.apple.com/kb/HT208050.
+MBP-de-Jose:~ joseconejerosivo$ python3
+Python 3.8.9 (default, Apr 13 2022, 08:48:07) 
+[Clang 13.1.6 (clang-1316.0.21.2.5)] on darwin
+Type "help", "copyright", "credits" or "license" for more information.
+>>> nombre = "Jose"
+>>> edad = 26
+>>> print(nombre)
+Jose
+>>> print(edad)
+26
+>>> type(nombre)
+<class 'str'>
+>>> type(edad)
+<class 'int'>
+>>> 
+
+
+
+
