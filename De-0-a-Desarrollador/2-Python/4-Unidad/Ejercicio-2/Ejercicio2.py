1e86c1e6bfded8f8e8911691ddee80c2a5f26f6a
JCSIVO <josecsivo@gmail.com>
2022-06-26 14:26:01 +0200

    Ejercicio1-2-3
    Ejercicios unidad 4 de python

---
+++ De-0-a-Desarrollador/2-Python/4-Unidad/Ejercicio-2/Ejercicio2.py
@@ -0,0 +1,5 @@
+#Mostrar los numeros impares desde el 2 hasta el 8
+
+for numeroImpar in range(2,8):
+    if numeroImpar % 2 !=0:
+        print(numeroImpar) 
---
+++ De-0-a-Desarrollador/2-Python/4-Unidad/Ejercicio-3/Ejercicio3.py
@@ -0,0 +1,4 @@
+numeros = range(1, 101)
+
+for numeros in sorted(numeros, reverse=True):
+    print(numeros)
