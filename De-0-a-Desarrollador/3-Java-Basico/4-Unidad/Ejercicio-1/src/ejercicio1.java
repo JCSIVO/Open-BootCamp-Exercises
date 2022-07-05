a1529d090d30636c1dc1a8105571c0d377cc06a6
JCSIVO <josecsivo@gmail.com>
2022-07-05 14:22:41 +0200

    Ejercicio unidad-4
    Ejercicio unidad 4 -Java básico

---
+++ De-0-a-Desarrollador/3-Java-Basico/4-Unidad/Ejercicio-1/src/ejercicio1.java
@@ -0,0 +1,16 @@
+
+
+public class ejercicio1 {
+    public static void main(String[] args) {
+        String [] nombres = {"Maria","Ester","Lucas","Pedro", "Juan", "Blanca"};
+
+        for(int i = 0; i < nombres.length; i++){
+            System.out.println("La mayor de las niñas es " + nombres[0] + " la segunda es " + nombres[1] + " el tercero es " +
+                    nombres[2] + " el cuarto es " + nombres[3] + " el quinto es " +nombres[4]+ " el sexto es "+ nombres[5]);
+
+        }
+
+    }
+}
+
+
