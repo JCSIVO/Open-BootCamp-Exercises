0036d857adcb16f433f9fdfc8ff8852c018e6437
JCSIVO <josecsivo@gmail.com>
2022-07-06 12:47:30 +0200

    Ejercicio1-java-basico
    Realización Ejercicio Unidad 6 Java Básico

---
+++ De-0-a-Desarrollador/3-Java-Basico/6-Unidad/Ejercicio-1/src/com/interfaces/CocheCRUD.java
@@ -0,0 +1,10 @@
+package com.interfaces;
+
+//Declaracion de los metodos y no se implementan
+//Dice lo que hay  que hacer pero no lo hace, solo lo indica
+
+public interface CocheCRUD {
+    void save();
+    void findAll();
+    void delete();
+}
