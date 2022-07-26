98f3f28709be862d814a43fc09791cb1092050e1
JCSIVO <josecsivo@gmail.com>
2022-07-26 14:10:50 +0200

    Ejercicio2-JavaScript
    Realización del Ejercicio de la Unidad 2

---
+++ De-0-a-Desarrollador/6-JavaScript-Básico/2-Unidad/Ejercicio1.js
@@ -0,0 +1,8 @@
+const my_lista = ["Jose", 28,true,new Date(1994,01,07),{
+    titulo: "El señor de los anillos",
+    autor: "J.K.ROWLING",
+    fecha: new Date(2010, 05,24),
+    url:"http:www.amazon.com"
+}]
+
+console.log('my_lista :_lista>> ', my);
