0e8d6666a79e4285537543488f59acc48f69832f
JCSIVO <josecsivo@gmail.com>
2022-07-26 19:51:00 +0200

    Ejercicios Unidad 3
    Realización de los ejercicios de la Unidad 3 de JavaScript Básico

---
+++ De-0-a-Desarrollador/6-JavaScript-Básico/3-Unidad/factorial-break.js
@@ -0,0 +1,10 @@
+let factorial = 1;
+let num = 10;
+while(true){
+    factorial *=num
+    num--
+    
+    if(num === 1)
+    break;
+}
+console.log(factorial);
---
+++ De-0-a-Desarrollador/6-JavaScript-Básico/3-Unidad/factorial-for.js
@@ -0,0 +1,7 @@
+//Calculo del factorial de 10
+
+let factorial = 1;
+for(i = 10; i > 0; i--){
+     factorial *= i;
+}
+console.log(factorial);
