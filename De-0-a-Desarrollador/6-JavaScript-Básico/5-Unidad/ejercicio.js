1bd0a4bfd4677b10be55f98135443425f350eb32
JCSIVO <josecsivo@gmail.com>
2022-07-27 16:35:57 +0200

    Ejercicio Unidad 5 JavaScrip
    Realización del Ejercicio Unidad 5 de JavaScript Básico

---
+++ De-0-a-Desarrollador/6-JavaScript-Básico/5-Unidad/ejercicio.js
@@ -0,0 +1,10 @@
+let altura_cm = 173;
+let altura_m = 1.73;
+let peso_dec = 80.24
+let altura_redon = Math.ceil(altura_m)
+console.log(altura_redon)
+let peso_redon = Math.floor(peso_dec)
+console.log(peso_redon)
+
+let igual = Number.MAX_VALUE + 1 === Number.MAX_VALUE
+console.log(igual)
