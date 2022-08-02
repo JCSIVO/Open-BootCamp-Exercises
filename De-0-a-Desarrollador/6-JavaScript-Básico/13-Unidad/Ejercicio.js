61a9bfedd27eecc0d99aff169b344409b4e9a3a6
JCSIVO <josecsivo@gmail.com>
2022-08-02 21:58:23 +0200

    Realización Ejercicio Unidad 13
    JavaScript Básico

---
+++ De-0-a-Desarrollador/6-JavaScript-Básico/13-Unidad/Ejercicio.js
@@ -0,0 +1,20 @@
+let nombre = "Jose";
+let apellido = "Conejero";
+let estudiante = nombre.concat(" ",apellido);
+console.log(estudiante)
+let estudianteMayus = estudiante.toUpperCase();
+let estudianteMinus = estudiante.toLowerCase();
+console.log("estudianteMayus")
+console.log(estudianteMinus)
+let num = estudiante.length;
+console.log(num)
+let pos = nombre[0]
+console.log(pos)
+let posult = apellido[apellido.length -1]
+console.log(posult)
+let elimiespa = estudiante.replace(" ","")
+console.log(elimiespa)
+let bol = estudiante.includes("pepe")
+console.log(bol)
+let bol2 = estudiante.includes("Jose")
+console.log(bol2)
