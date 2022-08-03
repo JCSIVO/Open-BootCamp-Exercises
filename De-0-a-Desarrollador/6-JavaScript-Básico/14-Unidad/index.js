c6dd2238988dcb5cc48513dbcbf4ade4e6f73a94
JCSIVO <josecsivo@gmail.com>
2022-08-03 08:58:07 +0200

    Ejercicio Unidad 14
    JavaScript Básico

---
+++ De-0-a-Desarrollador/6-JavaScript-Básico/14-Unidad/index.js
@@ -0,0 +1,9 @@
+let boton = document.getElementById("btn")
+
+boton.addEventListener("click", () => {
+    alert("Has hecho click en el boton")
+})
+
+$("button").click(function() {
+    console.log("Hola desde Jquery")
+})
