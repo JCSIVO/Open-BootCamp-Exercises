a000585d4094104d52209621b7c1b70113ea9d90
JCSIVO <josecsivo@gmail.com>
2022-08-08 17:28:51 +0200

    Ejercicio4-5-6
    ReactJS

---
+++ De-0-a-Desarrollador/7-ReactJS/ejer-6/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+import { ClockF } from '../src/Components/ClockF'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      
+        <ClockF></ClockF>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
