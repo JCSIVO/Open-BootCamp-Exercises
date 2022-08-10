097fdd28d4592b47231832849c9d571e9c4d4406
JCSIVO <josecsivo@gmail.com>
2022-08-10 11:41:54 +0200

    Ejercicio 9
    ReactJS

---
+++ De-0-a-Desarrollador/7-ReactJS/ejer-9/src/components/contactForm.jsx
@@ -0,0 +1,31 @@
+import React, {useRef} from 'react';
+
+const ContactForm = ({ onAddContact }) => {
+
+    const name = useRef('');
+    const email = useRef('');
+
+    function addContact(e){
+        e.preventDefault()
+
+        const newContact = {
+            nombre: name.current.value,
+            email: email.current.value,
+            conectado: true
+        }
+
+        onAddContact(newContact);
+        name.current.value= '';
+        email.current.value= ''; 
+    }
+    return (
+        <form onSubmit={addContact} className='contact-component'>
+            <h2>Añadir Contacto: </h2>
+            <input ref={name} name='name' placeholder='Nombre Contacto' className='form-control mb-2'></input>
+            <input ref={email} name='email' type='email' placeholder='Email Contacto'></input>
+            <button onClick={addContact} type='submit' className='submit-button'>Añadir</button>
+        </form>
+    );
+}
+
+export default ContactForm;
