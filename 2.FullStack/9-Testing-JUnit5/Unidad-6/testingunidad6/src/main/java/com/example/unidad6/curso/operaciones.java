c10caed9fd45f1c709b944f8288130b980ac165a
JCSIVO <josecsivo@gmail.com>
2022-09-25 20:43:01 +0200

    Unidad-6
    Test JUnit5 Open BootCamp

---
+++ 2.FullStack/9-Testing-JUnit5/Unidad-6/testingunidad6/src/main/java/com/example/unidad6/curso/operaciones.java
@@ -0,0 +1,14 @@
+package com.example.unidad6.curso;
+
+public class operaciones {
+    public double suma(double A, double B) {
+        return A + B;
+    }
+}
+
+class operacionesCondition {
+    static boolean mustBeRun() {
+        // return true;
+        return false;
+    }
+}
---
+++ 2.FullStack/9-Testing-JUnit5/Unidad-6/testingunidad6/src/test/java/com/example/unidad6/AllTest.java
@@ -0,0 +1,14 @@
+package com.example.unidad6;
+
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.suite.api.ExcludeTags;
+import org.junit.platform.suite.api.IncludeTags;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.runner.RunWith;
+
+@RunWith(JUnitPlatform.class)
+@SelectPackages("curso")
+// @IncludeTags("produccion")
+@ExcludeTags("produccion")
+public class AllTest {
+}
