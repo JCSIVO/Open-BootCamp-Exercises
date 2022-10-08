021de37cb8cac3cecafc00a7be8239190afbd24f
JCSIVO <josecsivo@gmail.com>
2022-10-09 01:27:54 +0200

    Unidad-15
    ReactJS Avanzado Open BootCamp

---
+++ 2.FullStack/10-ReactJS-Avanzado/Unidad-15/Unidad-15/ob-react-final-main/src/components/lists/TaskList.jsx
@@ -0,0 +1,104 @@
+import React, { useState } from 'react';
+
+/**
+ * Componente que gestiona la lista de tareas
+ *
+ * @returns {React.Component}
+ */
+
+const TaskList = () => {
+    const [newTask, setNewTask] = useState('');
+    const [Tasklist, setTasklist] = useState([]);
+
+  /**
+   * Añade una nueva tarea a la lista de tareas
+   * v2: La nueva tarea se añade como un objeto
+   * { task: nombre de la tarea, completed: Si esta cinpletada o no }
+   */
+
+    const addNewTask = () => {
+        setTasklist([...Tasklist, { task: newTask, completed: false }]);
+        setNewTask('');
+        return true;
+    };
+
+    /**
+     * Función para chequear si la lista de tareas esta vacía
+     * @returns true si taskslist.length === 0
+     */
+
+    const isTasksEmpty = () => Tasklist.length === 0;
+
+    /**
+     * Editar el nombre de la nueva tarea
+     * @param {*} e - Evento de onChange proveniente de React
+     */
+    const editNewItem = (e) => setNewTask(e.target.value);
+
+    /**
+     * Función para eliminar una tarea en concreto
+     * @param {*} index - índice de la tarea a eliminar
+     */
+    // const removeItem = (index) => {
+      // const newTasklist = Tasklist.filter((t, i) => i !== index);
+      // setTasklist(newTasklist);
+    // };
+
+    /**
+     * Cambia el item po completado <-> pendiente
+     * @param {*} index
+     */
+    const toggleCompletedItem = (index) => {
+      const newTaskList = Tasklist;
+      newTaskList[index].completed = !newTaskList[index].completed;
+      setTasklist([...newTaskList]);
+    };
+
+    /**
+     * Añade una nueva tarea cuando se presiona la tecla Enter
+     * @param {*} e - Evento onKeyDown que proviene por defecto de React
+     */
+    const insertNewItemOnEnterKey = (e) => e.key === 'Enter' && addNewTask();
+
+    return (
+      <div>
+        <h1>
+          Task List
+        </h1>
+        <div>
+          <input
+            className="input"
+            value={newTask}
+            onKeyDown={insertNewItemOnEnterKey}
+            onChange={editNewItem}
+            placeholder="New Task"
+            type="text"
+          />
+          <button className="btn" type="submit" onClick={addNewTask}>
+            Create Task
+          </button>
+        </div>
+        { isTasksEmpty()
+            ? (<p>Task List is Empty</p>)
+            : (
+              <ul>
+                {Tasklist.map((item, index) => (
+                  <li key={index}>
+                    <input
+                      type="checkbox"
+                      // onClick={() => removeItem(index)}
+                      onClick={() => toggleCompletedItem(index)}
+                      checked={item.completed}
+                      onChange={() => {}}
+                    />
+                    { item.task }
+                  </li>
+                        ))}
+              </ul>
+            )}
+
+      </div>
+    );
+};
+
+export default TaskList;
