be6c2381363661de5bbc7eb1d5f1f2ded27859e2
JCSIVO <josecsivo@gmail.com>
2022-10-16 17:13:42 +0200

    Ejercicio-1-C#
    Ruta de C# OpenBootCamp

---
+++ 3.Back-End-C#/2.C#/Ejercicio-1/Ejercicio-2/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+// Ejercicio 2
+// Escribe un programa que tome la hora y la escriba por pantalla 
+
+DateTime horaActual = new DateTime(2022, 10, 16, 17, 05, 37);
+Console.WriteLine("Hoy es " + horaActual);
