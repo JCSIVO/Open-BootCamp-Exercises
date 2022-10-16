3610b693ad4a3e319efc1ff14849242caa8c2295
JCSIVO <josecsivo@gmail.com>
2022-10-16 17:13:42 +0200

    Ejercicio-1-C#
    Ruta de C# OpenBootCamp

---
+++ 3.Back-End-C#/2.C#/Ejercicio-1/Ejercicio-1/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+// Ejercicio 1
+// Escribir un programa que reciba tu nombre y lo escriba por consola
+
+Console.WriteLine("¿Por favor introduce tu nombre?");
+string nombre = Console.ReadLine();
+Console.WriteLine("Buenos días " + nombre);
