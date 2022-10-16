d77355dedbd2646d8aa21778ec07af663fec151a
JCSIVO <josecsivo@gmail.com>
2022-10-16 17:11:59 +0200

    Unidad-1 C#
    Ruta de C# en OpenBootCamp

---
+++ 3.Back-End-C#/2.C#/Unidad-1/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.ReadLine();
+// string nombre = "JCSIVO";
+Console.WriteLine("Introduce tu nombre");
+string nombre = Console.ReadLine();
+
+
+Console.WriteLine("Hola, Mundo!" + " y " + nombre );
+
+// Edad?
+
+// Ciudad
+
+// Presentar mensaje "Hola, NOMBRE.  Tienes AÑOS. Vives en CIUDAD"
