409e08274674cb018f3d881e0d376edee4d4707b
JCSIVO <josecsivo@gmail.com>
2022-10-17 09:53:41 +0200

    Ejercicio-2 C#
    C# OpenBootCamp

---
+++ 3.Back-End-C#/2.C#/Ejercicio-2/Ejercicio-3/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+// Ejercicio 3
+// Determina los operadores para verificar las siguientes condiciones
+
+// Numero mayor o igual a 18
+Console.WriteLine("Introduce un numero");
+int numero = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Numero es mayor o igual que  18: " + (numero == 18 || numero >18));
+
+Console.WriteLine("Introduce una letra");
+char letra = Convert.ToChar(Console.ReadLine());
+Console.WriteLine("¿La letra es 'a'? " + (letra == 'a'));
+
+
+Console.WriteLine("Introduce un segundo numero");
+int numero2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("TRUE TRUE: " + (numero2 < 45 && numero2 > 60));
+
+
+Console.WriteLine("Introduce un tercer numero");
+int numero3 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("TRUE FALSE: " + (numero3 < 45 || numero3 < 60));
