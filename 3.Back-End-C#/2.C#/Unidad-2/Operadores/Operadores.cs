5e8aa645cc99ad633f80b68772db02395101a190
JCSIVO <josecsivo@gmail.com>
2022-10-17 09:52:58 +0200

    Unidad-2 C#
    C# OpenBootCamp

---
+++ 3.Back-End-C#/2.C#/Unidad-2/Operadores/Operadores.cs
@@ -0,0 +1,53 @@
+﻿// Operadores 
+// Aritméticos + - * / % ++ --
+
+int i  = 0;
+/*Console.WriteLine("Suma: " + (i + 1));
+
+// Autoincrementos 
+Console.WriteLine("Autoincremento: " + ++i);
+i = i + 1;
+Console.WriteLine("Autoincremento: " + --i);
+i = i - 1;
+Console.WriteLine("Valor: " + i);*/
+
+/*int i = 4;
+Console.WriteLine("División: " + i);
+Console.WriteLine("División: " + i / 4); // Resultado es entero*/
+
+// Operadores de Asignación
+
+// int j = 2;
+// int k = 1;
+// j = j + 3;
+// Console.WriteLine("Sumarle 3:" + j );
+// Console.WriteLine("Sumarle 3:" + (j+=3));
+// Console.WriteLine("j: " + j);
+
+// Operadores de comparación 
+/*Console.WriteLine("Iguales? " +(j == k));
+Console.WriteLine("DesIguales? " +(j != k));
+Console.WriteLine("j mayor? " +(j > k));
+Console.WriteLine("j menor? " +(j < k));*/
+
+// Operadores lógicos -> && || !
+int a = 9;
+int b = 11;
+// Y -AND
+Console.WriteLine("AND - Y");
+Console.WriteLine("TRUE TRUE: " + (a<10 && b>10));
+Console.WriteLine("FALSE TRUE: " + (a>10 && b>10));
+Console.WriteLine("TRUE FALSE: " + (a<10 && b<10));
+Console.WriteLine("FALSE FALSE: " + (a>10 && b<10));
+
+// O - OR
+Console.WriteLine("OR - O");
+Console.WriteLine("TRUE TRUE: " + (a<10 || b>10));
+Console.WriteLine("FALSE TRUE: " + (a>10 || b>10));
+Console.WriteLine("TRUE FALSE: " + (a<10 || b<10));
+Console.WriteLine("FALSE FALSE: " + (a>10 || b<10));
+
+// NO -NOT
+Console.WriteLine("NOT - NO");
+Console.WriteLine("TRUE: " + !(a<10));
+Console.WriteLine("FALSE: " + !(a>10));
---
+++ 3.Back-End-C#/2.C#/Unidad-2/Program.cs
@@ -0,0 +1,67 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+/*Console.WriteLine("Introduce tu nombre");
+string nombre = Console.ReadLine();
+
+// Edad ??
+Console.WriteLine("Introduce tu edad:");
+// string edad = Console.ReadLine();
+int edad = Convert.ToInt32(Console.ReadLine());
+
+// Ciudad ??
+Console.WriteLine("Introduce tu ciudad");
+string ciudad = Console.ReadLine();
+
+// Presentar mensaje "Hola, NOMBRE. Tienes AÑOS. Vives en ciudad"
+Console.Write("Hola, " + nombre);
+Console.Write(". Tienes " + edad);
+Console.Write(". Vives en  " + ciudad + ".");*/
+
+
+
+
+// Tipos de Datos y Variables
+
+/*int i = 0;
+float miFloat = i;
+long l = 1;
+
+float f = 1.555555f;
+double d = 2.666666666666d; 
+
+char c = 'c';
+string str = "string";*/
+
+// Console.Write("int: " + i );
+// Console.WriteLine(" miFloat: " + miFloat);
+/*Console.WriteLine("Long: " + l);
+
+Console.Write("float: " + f );
+Console.WriteLine("double: " + d);
+
+Console.Write("char: " + c );
+Console.WriteLine("string: " + str);*/
+
+
+/*string str2 = "32";
+int miInt2 = Convert.ToInt32(str2);
+Console.Write("str2: " + str2);
+miInt2 = miInt2 + 1;
+Console.WriteLine(" miInt2: " + miInt2);*/
+
+// const float pi = 3.141516f; // constante matemática`
+
+// float cualquierFloat = pi * 2;
+
+// pi = pi +1;
+
+// Nullables - Ejemplo
+// int? i = null;
+// int i = 0;
+// string? strNull = null; // no tiene valor
+// string strNull = ""; // Si tiene un valor vacio  
+
+// Console.WriteLine(i.HasValue); // null -> false <-> valor true
+// i = 1;
+// Console.WriteLine(i + 1);
