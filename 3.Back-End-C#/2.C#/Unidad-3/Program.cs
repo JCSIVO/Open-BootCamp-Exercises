dd6a5dd86f5c5cff436b8ed7a9fb269caaeb9512
JCSIVO <josecsivo@gmail.com>
2022-10-17 13:32:56 +0200

    Unidad-3 C#
    C# -> OpenBootCamp

---
+++ 3.Back-End-C#/2.C#/Unidad-3/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+// Estructuras
+
+Coords misCoordenadas = new Coords(2,10, 7);
+Console.WriteLine("X: " + misCoordenadas.X);
+Console.WriteLine("Y: " + misCoordenadas.Y);
+Console.WriteLine("Z: " + misCoordenadas.Z);
+
+Coords misCoordenadas2 = misCoordenadas with { X = 3 };
+Console.WriteLine(misCoordenadas2);
+public struct Coords 
+{
+    public Coords(double x, double y, double z)
+    {
+        X = x;
+        Y = y;
+        Z = z;
+    }
+    public double X {get; set;} // Con get es solo lectura y con set es para lectura escritura
+    public double Y {get; set;}
+
+    public double Z {get; set;}
+
+    public override string ToString() => $"({X},{Y}, {Z})";
+} 
