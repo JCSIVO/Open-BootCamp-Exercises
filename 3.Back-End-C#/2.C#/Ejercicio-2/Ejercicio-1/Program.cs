c56c83381ebd60c240177cb5d2867cf41c297e10
JCSIVO <josecsivo@gmail.com>
2022-10-17 09:53:41 +0200

    Ejercicio-2 C#
    C# OpenBootCamp

---
+++ 3.Back-End-C#/2.C#/Ejercicio-2/Ejercicio-1/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+
+
+// Ejercicio 1
+// Programa que reciba datos de una persona y genere un mensaje, usa una 
+// variable para cada dato y otra para el mensaje. EJ: nombre, apellido, edad, sabe programar
+
+Console.WriteLine("Buenos días, ¿Me puede indicar cuales son sus datos?");
+
+Console.WriteLine("Por favor intrdocuce tu nombre");
+string nombre = Console.ReadLine();
+
+Console.WriteLine("Por favor intrdocuce tus apellidos");
+string apellidos = Console.ReadLine();
+
+Console.WriteLine("¿Cual es tu edad?");
+int edad = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("¿Sabes programar (responder: True / False)?");
+bool programar = Convert.ToBoolean(Console.ReadLine());
+
+
+Console.WriteLine("Hola " + nombre + " " + apellidos + " tu edad es de " + edad + " años. " + "¿Sabes programar? " + programar);
+
+
---
+++ 3.Back-End-C#/2.C#/Ejercicio-2/Ejercicio-2/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+Console.WriteLine("¿Que vehículo quieres describir?");
+String vehiculo = Console.ReadLine();
+Console.WriteLine("¿Cuantas puertas tiene?");
+int puerta = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("¿Cuantas ruedas tiene?");
+int ruedas = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("¿Que marca de vehículo es?");
+string marca = Console.ReadLine();
+Console.WriteLine("¿Tiene la ITV en vigor (respuesta:True ó False)?");
+bool itv =Convert.ToBoolean(Console.ReadLine()); 
+
+Console.WriteLine("Es un vehiculo " + vehiculo + " tiene " + puerta + " y " + ruedas + " su marca es " + marca + " ITV en vigor " + itv);
+
+
+
+Console.WriteLine("Describir caracterisiticas de la mesa");
+String mesa = Console.ReadLine();
+Console.WriteLine("¿Cuanto pesa?");
+float peso = Convert.ToSingle(Console.ReadLine());
+Console.WriteLine("¿que dimensiones tiene?");
+float largo = Convert.ToSingle(Console.ReadLine());
+Console.WriteLine("¿De que material esta hecho?");
+string material = Console.ReadLine();
+Console.WriteLine("y su color es:");
+string color = Console.ReadLine();
+
+
+
+Console.WriteLine("Es un vehiculo " + mesa + " pesa " + peso + " KG " + largo + " cm de largo y esta hecho de " + material + " y su color es: " + color);
