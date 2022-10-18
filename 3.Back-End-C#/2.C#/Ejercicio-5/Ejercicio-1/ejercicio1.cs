99288628902d1cc17473801fdf7f9d00bb7270da
JCSIVO <josecsivo@gmail.com>
2022-10-18 22:49:01 +0200

    Ejercicio-5-C#
    Ruta C# -> OpenBootCamp

---
+++ 3.Back-End-C#/2.C#/Ejercicio-5/Ejercicio-1/ejercicio1.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+// Ejercicio 1 - If
+
+// Escribe un programa que:
+// Pida datos a un cliente: Nombre, Email, Cupón.
+// Determine si un cliente tiene un cupón de descuento
+// Muestre un precio rebajado en función del descuento
+// Muestre el precio de un producto sin descuento si no hay cupón
+
+float precio = 16f;
+Cliente cliente = new Cliente(); 
+
+Console.WriteLine("Por favor introduce tu nombre");
+cliente.Nombre = Console.ReadLine();
+
+Console.WriteLine("Introduce el email");
+cliente.Email = Console.ReadLine();
+
+Console.WriteLine("¿Tienes Cupón? (True / False)");
+cliente.Cupon = Convert.ToBoolean(Console.ReadLine());
+
+Console.WriteLine(cliente);
+
+if (cliente.Cupon == true)
+{
+    Console.WriteLine($"El precio del producto es {precio} pero con el cupon te cuesta {precio/2}");
+}
+else
+{
+    Console.WriteLine($"el producto te cuesta {precio}");
+}
+public struct Cliente
+{
+    public Cliente(string nom, string email, bool cupon)
+    {
+        Nombre = nom;
+        Email = email;
+        Cupon = cupon;
+    }
+    public string Nombre {get; set;}
+    public string Email {get; set;}
+    public bool Cupon {get; set;}
+
+    public override string ToString() => $"({Nombre}, {Email}, {Cupon})";
+}
---
+++ 3.Back-End-C#/2.C#/Ejercicio-5/Ejercicio-2/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+// Ejercicio 2 - Switch
+
+// Haz una lista de lenguajes de programación, por ejemplo: C#, Java, C++,.
+// Presenta la lista en consola y pide que el usuario seleccione el lenguaje mediante
+// 1,2,3 oa,b,c.Presenta el resultado en consola.
+// Nota: Puedes añadir al resultado el "Hola, mundo" para el caso de C#.
+
+List<string> lenguajesProgramación = new List<string>()
+{
+    "0 - Python",
+    "1 - Java",
+    "2 - JavaScript",   
+    "3 - C#"
+};
+foreach (var item in lenguajesProgramación)
+{
+    Console.WriteLine(item);
+}
+
+Console.WriteLine("Introduce un valor numerico");
+int valor  = Convert.ToInt32(Console.ReadLine());
+
+switch (++valor)
+{
+    case 1:
+        Console.WriteLine("Python");
+        break;
+    case 2:
+        Console.WriteLine("Java");
+        break;
+    case 3:
+        Console.WriteLine("JavaScript");
+        break;
+    case 4:
+        Console.WriteLine("C# = Hola, mundo");
+        break;
+    default:
+        Console.WriteLine("No conozco ese lenguaje");
+        break;
+}
