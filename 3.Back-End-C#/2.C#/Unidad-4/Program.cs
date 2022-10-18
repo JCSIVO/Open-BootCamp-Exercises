8ef087b6d551de901f436cdd752832d038d28c2d
JCSIVO <josecsivo@gmail.com>
2022-10-18 10:48:19 +0200

    Unidad-4-C#
    C# -> Open BootCamp

---
+++ 3.Back-End-C#/2.C#/Unidad-4/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+// Bucles
+// WHILE - Mientras
+/*int i = 1;
+Console.WriteLine("\nWHILE: ");
+while (i <= 10) // Sin el igual es de 0-9 con el igual va de 0-10
+{
+     // Código que se ejecuta mientras "True"
+    // Console.WriteLine("Estamos dentro del bucle");
+    Console.Write(i + " ");
+    i++; // i = i+1
+}
+Console.WriteLine("\nDespués del while" + i); // i vale 11
+
+// FOR - PARA / POR
+Console.WriteLine("\nFOR:");
+for (int j = 1; j <=10; j++) // asignando 1 a la i
+{
+    Console.Write(j + " ");
+}
+
+
+// DO...WHILE - HACER...MIENTRAS
+Console.WriteLine("\nDO...WHILE:");
+i = 5;
+do
+{
+    Console.Write(i + " ");
+    i++; // i += 5 -> i = i + 5
+} while (i <= 10);
+Console.WriteLine("");
+// FOREACH - PARA/POR CADA
+Console.WriteLine("\nFOREACH:");
+
+var lista = new List<string>{ "Blanca", "Lucas", "Isabel", "Irene" };
+foreach (var elemento in lista)
+{
+    Console.WriteLine(elemento);
+}
+*/
+
+
