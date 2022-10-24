eceb86acd6a52d2c19e3d4def77521b99a4f8b22
JCSIVO <josecsivo@gmail.com>
2022-10-24 13:56:32 +0200

    Unidad-16-C#
    Ruta C# OpenBootCamp

---
+++ 3.Back-End-C#/2.C#/Unidad-16/EjemploJSON/JSON.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+using System.Net;
+// Conseguir un JSON de un API y usar su contenido
+
+/*
+* Creamos un request y un response para obtener el JSON
+* request = peticion al servidor del API
+* response = guardamos los datos que obenemos de la petición 
+*/
+HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://reqres.in/api/users?page=2");
+HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+// Creamos un stream
+Stream stream = response.GetResponseStream();
+StreamReader reader = new StreamReader(stream);
+
+var json = reader.ReadToEnd();
+Console.WriteLine(json);
