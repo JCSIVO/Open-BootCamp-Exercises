e0fa1b855fadd88179061f9d3d50255350b4c41e
JCSIVO <josecsivo@gmail.com>
2022-10-25 17:56:20 +0200

    Unidad-21-C#
    Ruta C# OpenBootCamp

---
+++ 3.Back-End-C#/2.C#/Unidad-21/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System.Diagnostics;
+
+// Sort y Complejidad
+
+int[] arr; 
+Console.WriteLine("Introduce numero de elementos");
+int N = Convert.ToInt32(Console.ReadLine());
+// int N = 1000;
+arr = new int[N];
+
+for (int i = 0; i < N; i++)
+{
+    arr[i] = 1;
+}
+Stopwatch crono = new Stopwatch();
+crono.Start();
+for (int j = 0; j < N; j++)
+{
+    for (int k = N-1; k > 0; k--)
+    {
+        arr[k] = j + k;
+    }
+}
+crono.Stop();
+Console.WriteLine($"Para {N} elementos, se tarda: {crono.Elapsed} segundos");
+
+
+/* N  Complejidad n^2
+* 1000
+* 10000 
+* 100000 00:00:00.2657275
+*/
