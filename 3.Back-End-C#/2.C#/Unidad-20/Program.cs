fa73756cd84363e9b1707f302b9e2979756c6fb0
JCSIVO <josecsivo@gmail.com>
2022-10-25 17:55:29 +0200

    Unidad-20-C#
    Ruta C# OpenBootCamp

---
+++ 3.Back-End-C#/2.C#/Unidad-20/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+using System.Diagnostics;
+// Depuración o Debugging, Debug
+// Bug -> "error", "typo", "resultado incorrecto", etc...
+
+Console.WriteLine("Escribe tu mensaje:");
+var msg = Console.ReadLine();
+Console.WriteLine($"He recibido este mensaje: {msg}");
+int resultado = 0;
+Stopwatch sw = new Stopwatch();
+sw.Start();
+for (int i = 0; i < 10; i++)
+{
+    resultado = i * i +1;
+    // Console.WriteLine(resultado); // Error -> i^2 + 1
+    // Operaciones que queremos controlar 
+}
+sw.Stop();
+var tiempo = sw.Elapsed;
+Console.ReadLine();
