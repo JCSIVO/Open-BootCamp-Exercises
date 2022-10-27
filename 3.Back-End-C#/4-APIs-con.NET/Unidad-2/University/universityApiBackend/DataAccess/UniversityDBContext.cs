0ea498c475c4c34cb2d758e34b038849a8b55656
JCSIVO <josecsivo@gmail.com>
2022-10-27 17:02:16 +0200

    Unidad-2-.Net
    Ruta C# OpenBootCamp

---
+++ 3.Back-End-C#/4-APIs-con.NET/Unidad-2/University/universityApiBackend/DataAccess/UniversityDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using universityApiBackend.Models.DataModels;
+
+namespace universityApiBackend.DataAccess
+{
+    public class UniversityDBContext : DbContext
+    {
+        public UniversityDBContext(DbContextOptions<UniversityDBContext> options) : base(options) 
+        {
+
+        }
+        // Add DbSets (Tables of our Data base)
+        public DbSet<User>? Users { get; set; }
+    }
+}
