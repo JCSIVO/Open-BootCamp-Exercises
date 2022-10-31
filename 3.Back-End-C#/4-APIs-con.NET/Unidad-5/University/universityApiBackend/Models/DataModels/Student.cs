dca121ca5c17f0c0c6297e62c1045ffb8529921d
JCSIVO <josecsivo@gmail.com>
2022-10-31 09:16:26 +0100

    Unidad-5-NET
    Ruta C# Net OpenBootCamp

---
+++ 3.Back-End-C#/4-APIs-con.NET/Unidad-5/University/universityApiBackend/Models/DataModels/Student.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace universityApiBackend.Models.DataModels
+{
+    public class Student :BaseEntity
+    {
+        [Required]
+        public string Name { get; set; } = string.Empty;
+        [Required]
+        public string LastName { get; set; } = string.Empty;
+        [Required]
+        public DateTime Dob { get; set; }
+
+    public ICollection<Course>  Courses { get; set; } = new List<Course>();
+
+    }
+}
