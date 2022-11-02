d417763ee0d5f2afc9661492ea7f1495aec2e6e4
JCSIVO <josecsivo@gmail.com>
2022-11-02 09:03:48 +0100

    Ejercicio-6-Net
    Ruta C# NET OpenBootCamp

---
+++ 3.Back-End-C#/4-APIs-con.NET/Ejercicio-6/University/universityApiBackend/Program.cs
@@ -0,0 +1,116 @@
+// 1. Using to work with EntityFramework
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using universityApiBackend;
+using universityApiBackend.DataAccess;
+using universityApiBackend.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+// 2. Conection with SQL Server Express
+const string CONNECTIONNAME = "UniversityDB";
+var connectionString = builder.Configuration.GetConnectionString(CONNECTIONNAME);
+
+
+// 3. Add Context to service of builder
+builder.Services.AddDbContext<UniversityDBContext>(options =>options.UseSqlServer(connectionString));
+
+
+
+// 7. Add Service of JWT Autorization
+builder.Services.AddJwtTokenServices(builder.Configuration);
+
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+// 4. Add Custom Services ( folder Services)
+builder.Services.AddScoped<IStudentsService, StudentsService>();
+// TODO: Add the rest of services 
+
+
+// 8. Add Authorization
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("UserOnlyPolicy", policy => policy.RequireClaim("UserOnly", "User1"));
+});
+
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+
+// 9. Config Swagger to take care of Autorization of JWT
+
+builder.Services.AddSwaggerGen( options =>
+{
+    // We define the Security for authorization
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "JWT Authorization Header using Bearer Scheme"
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+        {
+            Reference = new OpenApiReference
+            {
+                Type = ReferenceType.SecurityScheme,
+                Id = "Bearer"
+            }
+        },
+        new string[]{ }
+        }
+        
+    });
+
+});
+
+
+// 5. CORS Configuration
+builder.Services.AddCors(options =>
+    {
+        options.AddPolicy(name: "CorsPolicy", builder =>
+        {
+            builder.AllowAnyOrigin();
+            builder.AllowAnyMethod();
+            builder.AllowAnyHeader();
+        });
+    });
+
+
+
+
+
+
+
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+// 6. Tell app to use CORS 
+app.UseCors("CorsPolicy");
+
+app.Run();
