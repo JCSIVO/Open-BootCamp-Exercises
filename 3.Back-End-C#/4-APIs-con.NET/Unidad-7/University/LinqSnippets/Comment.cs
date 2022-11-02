a9c1541b086b6c9fdba8364ea9a4f136ccd1e8d2
JCSIVO <josecsivo@gmail.com>
2022-11-02 09:02:39 +0100

    Unidad-7-Net
    Ruta C# NET OpenBootCamp

---
+++ 3.Back-End-C#/4-APIs-con.NET/Unidad-7/University/LinqSnippets/Comment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqSnippets
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+
+        public DateTime Created { get; set; }
+        
+
+    }
+}
---
+++ 3.Back-End-C#/4-APIs-con.NET/Unidad-7/University/universityApiBackend/AddJwtTokenServicesExtensions.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using universityApiBackend.Models.DataModels;
+
+namespace universityApiBackend
+{
+    public static class AddJwtTokenServicesExtensions
+    {
+        public static void AddJwtTokenServices(this IServiceCollection Services, IConfiguration configuration)
+        {
+            // Add JWT Settings 
+            var bindJwtSettings = new JwtSettings();
+            configuration.Bind("JsonWebTokenKeys", bindJwtSettings );
+
+            // Add Singleton of JWT Settings
+            Services.AddSingleton(bindJwtSettings);
+
+            Services
+                .AddAuthentication(options =>
+                {
+                    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                })
+                .AddJwtBearer(options =>
+                {
+                    options.RequireHttpsMetadata = false;
+                    options.SaveToken = true;
+                    options.TokenValidationParameters = new TokenValidationParameters()
+                    {
+                        ValidateIssuerSigningKey = bindJwtSettings.ValidateIssuerSigningKey,
+                        IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(bindJwtSettings.IssuerSigningKey)),
+                        ValidateIssuer = bindJwtSettings.ValidateIssuer,
+                        ValidIssuer = bindJwtSettings.ValidIssuer,
+                        ValidateAudience = bindJwtSettings.ValidateAudiende,
+                        ValidAudience = bindJwtSettings.ValidAudience,
+                        RequireExpirationTime = bindJwtSettings.RequireExpirationTime,
+                        ValidateLifetime = bindJwtSettings.ValidateLifeTime,
+                        ClockSkew = TimeSpan.FromDays(1)
+                    };
+                });
+        }
+    }
+}
