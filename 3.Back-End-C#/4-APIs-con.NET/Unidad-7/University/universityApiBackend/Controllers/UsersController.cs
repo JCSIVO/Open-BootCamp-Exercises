4ec775a8313176f79724edffe2600939c16062d0
JCSIVO <josecsivo@gmail.com>
2022-11-02 09:02:39 +0100

    Unidad-7-Net
    Ruta C# NET OpenBootCamp

---
+++ 3.Back-End-C#/4-APIs-con.NET/Unidad-7/University/universityApiBackend/Controllers/UsersController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using universityApiBackend.DataAccess;
+using universityApiBackend.Models.DataModels;
+
+namespace universityApiBackend.Controllers
+{
+    [Route("api/[controller]")] // Controller for Request to https://localhost:7208/api/Users
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly UniversityDBContext _context;
+
+        public UsersController(UniversityDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: https://localhost:7208/api/Users
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        // GET: https://localhost:7208/api/Users/1
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUser(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return user;
+        }
+
+        // PUT: https://localhost:7208/api/Users/1
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutUser(int id, User user)
+        {
+            if (id != user.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(user).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST:  https://localhost:7208/api/Users
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<User>> PostUser(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetUser", new { id = user.Id }, user);
+        }
+
+        // DELETE: https://localhost:7208/api/Users/1
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUser(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool UserExists(int id)
+        {
+            return _context.Users.Any(user => user.Id == id);
+        }
+    }
+}
