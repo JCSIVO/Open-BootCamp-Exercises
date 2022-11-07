aad6212fa86371680989de11e89b4835a8881284
JCSIVO <josecsivo@gmail.com>
2022-11-07 07:19:26 +0100

    Unidad-10-Net
    Ruta C# Net OpenBootCamp

---
+++ 3.Back-End-C#/4-APIs-con.NET/Unidad-9/University/universityApiBackend/Models/DataModels/BaseEntity.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace universityApiBackend.Models.DataModels
+{
+    public class BaseEntity
+    {
+        [Required]
+        [Key]
+        public int Id { get; set; }
+        // public int UserID { get; set; }
+        public string CreateBy { get; set; } = string.Empty;
+        public DateTime CreateAt { get; set; } = DateTime.Now;
+        public string UpdateBy { get; set; } = string.Empty;
+        public DateTime? UpdateAt { get; set; }
+        public string DeleteBy { get; set; } = string.Empty;
+        public DateTime? DeleteAt { get; set; }
+        public bool IsDeleted { get; set; } = false;
+    }
+}
---
+++ 3.Back-End-C#/4-APIs-con.NET/Unidad-9/University/universityApiBackend/Models/DataModels/UserTokens.cs
@@ -0,0 +1,14 @@
+﻿namespace universityApiBackend.Models.DataModels
+{
+    public class UserTokens
+    {
+        public int Id { get; set; }
+        public string Token { get; set; }
+        public string UserName { get; set; }
+        public TimeSpan Validity { get; set; }
+        public string RefreshToken { get; set; }
+        public string EmailId { get; set; }
+        public Guid GuidId { get; set; }
+        public DateTime ExpiredTime { get; set; }
+    }
+}
