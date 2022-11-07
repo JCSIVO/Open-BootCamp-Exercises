eea0e4e324df4774f0865894647795c538d2917c
JCSIVO <josecsivo@gmail.com>
2022-11-07 13:47:34 +0100

    Unidad-11-NET
    Ruta C# NET OpenBootCamp

---
+++ 3.Back-End-C#/4-APIs-con.NET/Unidad-11/University/universityApiBackend/Migrations/20221027140320_Create Users table.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace universityApiBackend.Migrations
+{
+    public partial class CreateUserstable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreateBy = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreateAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdateBy = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UpdateAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DeleteBy = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DeleteAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
---
+++ 3.Back-End-C#/4-APIs-con.NET/Unidad-11/University/universityApiBackend/Services/StudentsService.cs
@@ -0,0 +1,18 @@
+﻿using universityApiBackend.Models.DataModels;
+
+namespace universityApiBackend.Services
+{
+    public class StudentsService : IStudentsService
+    {
+        // TODO: Resolve Methods  
+        public IEnumerable<Student> GetStudentsWithCourses()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Student> GetStudentsWithNoCourses()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
