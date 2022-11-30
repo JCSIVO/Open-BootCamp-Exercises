3e69afd1fb5c3aa03ced51e33894f9267133a076
JCSIVO <josecsivo@gmail.com>
2022-11-30 14:54:01 +0100

    Unidad-1-Symfony
    Ruta PHP Symfony Open BootCamp

---
+++ 4.Back-End-PHP/5.Symfony/Unidad-1/01.inicio_rápido/02.ejemplo_de_instalación_website/symfony/src/Controller/FirstController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FirstController extends AbstractController
+{
+    #[Route('/first', name: 'app_first')]
+    public function index(): Response
+    {
+        return $this->render('first/index.html.twig', [
+            'controller_name' => 'FirstController',
+        ]);
+    }
+
+    
+}
