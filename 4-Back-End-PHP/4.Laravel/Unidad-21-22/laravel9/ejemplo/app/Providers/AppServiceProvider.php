13b82f42fcaae6279f919304b3ccef83639c6ebf
JCSIVO <josecsivo@gmail.com>
2026-08-10 23:58:47 +0200

    Update Vulnerabilities
    Update Vulnerabilities

--- 4-Back-End-PHP/4.Laravel/Unidad-21-22/laravel9/ejemplo/app/Providers/AppServiceProvider.php
+++ 4-Back-End-PHP/4.Laravel/Unidad-21-22/laravel9/ejemplo/app/Providers/AppServiceProvider.php
@@ -2,27 +2,34 @@
 
 namespace App\Providers;
 
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
-     *
-     * @return void
      */
-    public function register()
+    public function register(): void
     {
         //
     }
 
     /**
      * Bootstrap any application services.
-     *
-     * @return void
      */
-    public function boot()
+    public function boot(): void
     {
-        //
+        Route::resourceVerbs([
+            'create' => 'crear',
+            'edit' => 'editar',
+        ]);
+
+        RateLimiter::for('api', function (Request $request) {
+            return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
+        });
     }
 }
